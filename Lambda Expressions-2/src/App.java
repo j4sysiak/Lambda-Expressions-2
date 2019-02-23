1a02648a99b79d04cf6b41281947f1f5ef71bea7
j4sysiak <j4sysiak@gmail.com>
2019-02-23 06:40:53 +0100

    3

--- Lambda Expressions-2/src/App.java
+++ Lambda Expressions-2/src/App.java
@@ -14,12 +14,13 @@ public class App {
 
 	public static void main(String[] args) {
 
-		Runner runner = new Runner();
+		Runner  runner  = new Runner();
 		Runner2 runner2 = new Runner2();
 		
 		System.out.println("=================depricated (java 1.6 - 1.7)=======================");
 		
-		runner2.run(/*implementing object interface Executable*/ new Executable2() {
+		runner2.run(
+			/*implementing object interface Executable*/ new Executable2() {
 			
 			@Override
 			public void execute() {
@@ -38,17 +39,19 @@ public class App {
 				
 			}
 
-		}
+			}
 					 );
 		
 		System.out.println("==================Lambda (java 1.8)======================");
 		
 		runner.run(/*implementing object interface Executable*/ () -> System.out.println("Lambda expression1") );
 		
-		runner.run(/*implementing object interface Executable*/ () -> {
+		runner.run(/*implementing object interface Executable*/ 
+					() -> {
 							System.out.println("Lambda expression2");
 							System.out.println("Lambda expression3");
-						  } );
+						  } 
+				  );
 		
 		
 		//runner2.run( () -> System.out.println("Lambda2 expression") );
